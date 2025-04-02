47e4fa32022433d45f09e9066b9767b235d0aad0
Aaleen <aaleenmirza110@gmail.com>
2025-04-02 19:48:46 +0530

    prompt2 changes: Add sounds, images, or polish the game with smoother animations and scoring systems. also provide me with the list of assets which will be required with new code

--- GamePanel.java
+++ GamePanel.java
@@ -3,6 +3,9 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
+import java.io.File;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
     private Timer timer;
@@ -13,19 +16,36 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
     private Random random;
     private int score = 0;
     private boolean gameOver = false;
-
+    private Image birdImage, pipeImage, backgroundImage;
+    
     public GamePanel() {
+        loadAssets();
         timer = new Timer(20, this);
         random = new Random();
         pipes = new ArrayList<>();
-        addPipe();
-        addPipe();
-        addPipe();
+        addPipe(); addPipe(); addPipe();
         timer.start();
         addKeyListener(this);
         setFocusable(true);
     }
-
+    
+    private void loadAssets() {
+        birdImage = new ImageIcon("assets/bird.png").getImage();
+        pipeImage = new ImageIcon("assets/pipe.png").getImage();
+        backgroundImage = new ImageIcon("assets/background.png").getImage();
+    }
+    
+    private void playSound(String filename) {
+        try {
+            File soundFile = new File("assets/" + filename);
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(soundFile));
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
     private void addPipe() {
         int pipeHeight = 50 + random.nextInt(300);
         int gap = 150;
@@ -33,69 +53,67 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
         pipes.add(new Rectangle(x, 0, 80, pipeHeight));
         pipes.add(new Rectangle(x, pipeHeight + gap, 80, 600 - pipeHeight - gap));
     }
-
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         if (!gameOver) {
             velocity += gravity;
             birdY += velocity;
-
-            // Move pipes
+            
             for (Rectangle pipe : pipes) {
                 pipe.x -= 5;
             }
-
-            // Remove pipes that are off-screen and add new ones
+            
             if (pipes.get(0).x + pipes.get(0).width < 0) {
                 pipes.remove(0);
                 pipes.remove(0);
                 addPipe();
                 score++;
+                playSound("point.wav");
             }
-
-            // Collision detection
+            
             for (Rectangle pipe : pipes) {
-                if (pipe.intersects(new Rectangle(100, birdY, 30, 30))) {
+                if (pipe.intersects(new Rectangle(100, birdY, 34, 24))) {
                     gameOver = true;
+                    playSound("hit.wav");
                 }
             }
-
+            
             if (birdY > getHeight() || birdY < 0) {
                 gameOver = true;
+                playSound("hit.wav");
             }
-
+            
             repaint();
         }
     }
-
+    
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.cyan);
-        g.fillRect(0, 0, getWidth(), getHeight());
-
-        g.setColor(Color.orange);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), null);
+        
         for (Rectangle pipe : pipes) {
-            g.fillRect(pipe.x, pipe.y, pipe.width, pipe.height);
+            g.drawImage(pipeImage, pipe.x, pipe.y, pipe.width, pipe.height, null);
         }
-
-        g.setColor(Color.red);
-        g.fillRect(100, birdY, 30, 30);
-
+        
+        g.drawImage(birdImage, 100, birdY, 34, 24, null);
+        
         g.setColor(Color.black);
         g.setFont(new Font("Arial", Font.BOLD, 32));
         g.drawString("Score: " + score, 10, 40);
-
+        
         if (gameOver) {
-            g.setColor(Color.black);
+            g.setColor(Color.red);
             g.drawString("Game Over! Press R to restart", 200, 300);
         }
     }
-
+    
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_SPACE && !gameOver) {
             velocity = -10;
+            playSound("flap.wav");
         }
         if (e.getKeyCode() == KeyEvent.VK_R && gameOver) {
             birdY = 300;
@@ -103,15 +121,12 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
             pipes.clear();
             score = 0;
             gameOver = false;
-            addPipe();
-            addPipe();
-            addPipe();
+            addPipe(); addPipe(); addPipe();
         }
     }
-
+    
     @Override
     public void keyTyped(KeyEvent e) {}
-
     @Override
     public void keyReleased(KeyEvent e) {}
 }
